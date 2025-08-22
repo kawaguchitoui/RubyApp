ecb6f7cf4bd390b4c862e7aa1102e08da8d2d845
kawaguchitoui <83398093+kawaguchitoui@users.noreply.github.com>
2025-08-22 14:19:41 +0900

    Update index.rb

--- ruby_basic/index.rb
+++ ruby_basic/index.rb
@@ -1,10 +1,12 @@
 class Car
   def initialize(car_name, car_model, capacity)
+    # インスタンス変数（インスタンスごとに保持される）
     @name = car_name
     @model = car_model
     @capacity = capacity
   end
 
+  # クラスメソッド（インスタンスを作らなくても呼び出せる）
   def self.run
     puts '走行します'
   end
@@ -21,7 +23,6 @@ end
 
 fire_truck = Car.new('消防車', '大型車', 5)
 fire_truck.ride_car(3)
-
 Car.run
 
 
@@ -29,6 +30,7 @@ Car.run
 def add_friend(list)
   friend = {}
   puts "お友達の名前は？"
+  # 文字列の末尾から改行文字を取り除く
   friend[:name] = gets.chomp
   puts "お友達の身長は？"
   friend[:height] = gets.to_i
@@ -39,7 +41,6 @@ end
 
 def judge_height(list)
   ride_count = 0
-
   list.each do |friend|
     if friend[:height] >= 130
       puts "#{friend[:name]}くんは乗車できます"
@@ -48,17 +49,13 @@ def judge_height(list)
       puts "#{friend[:name]}くんは乗車できません！"
     end
   end
-
   puts "乗車するのは#{ride_count}人です"
 end
 
 
 
-ride_count = 0
 friends = []
-
 3.times do
   friends = add_friend(friends)
 end
-
 judge_height(friends)
